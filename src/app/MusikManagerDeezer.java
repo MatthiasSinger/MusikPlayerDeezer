e85b774948a4888030c37a98c3aa2cb37b24aad6
MatthiasSinger <matze.singer@gmail.com>
2017-06-12 18:30:25 +0200

    Icon entfernt da Compile Problem

--- src/app/MusikManagerDeezer.java
+++ src/app/MusikManagerDeezer.java
@@ -1,6 +1,22 @@
 package app;
 
-public class MusikManagerDeezer
-{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class MusikManagerDeezer extends Application
+{
+	public void start(Stage primaryStage) throws Exception
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("/app/views/Main.fxml"));
+		primaryStage.setScene(new Scene(root));
+	    primaryStage.show();
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 }
